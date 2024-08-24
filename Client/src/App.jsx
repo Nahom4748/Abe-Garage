cf2ffa5ae90e3b281bedff4881527e638d89b798
Nahom4748 <155730526+Nahom4748@users.noreply.github.com>
2024-08-24 23:21:53 +0300

    First Commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import HeroPage from "./Components/HeroPage/HeroPage";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeroPage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
