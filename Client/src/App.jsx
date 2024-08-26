fa43af41eabfd329fefed32348d8dbf80b91c6bb
Adane-Dane <cocadane4@gmail.com>
2024-08-26 16:18:01 +0300

    Services

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import HeroPage from "./Components/HeroPage/HeroPage";
+import Services from "./Components/Services/Services";
 
 function App() {
   return (
     <>
       <Header />
       <HeroPage />
+      <Services />
       <Footer />
     </>
   );
