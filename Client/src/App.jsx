5c43a6ab8c889754346498fccc217ece6c85e7cd
Nahom4748 <155730526+Nahom4748@users.noreply.github.com>
2024-08-27 12:39:09 +0300

    login_and_Register

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import HeroPage from "./Components/HeroPage/HeroPage";
 import Services from "./Components/Services/Services";
+
 import "./assets/css/coming-soon.css";
 import "./assets/css/css-slide.css";
 import "./assets/css/star-rating-svg.css";
@@ -12,12 +13,16 @@ import "./assets/css/swiper.css";
 import "./assets/css/templete.css";
 import "./assets/css/templete.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
     <>
       <Header />
       <HeroPage />
+      <Login />
+      <Register />
       <AboutUs />
       <Services />
       <Footer />
