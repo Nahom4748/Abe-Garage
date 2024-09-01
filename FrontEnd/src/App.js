9cfde9fa0896ad93ed9d966bcd7a5a8a8e62a6d0
Nahom4748 <155730526+Nahom4748@users.noreply.github.com>
2024-09-01 13:23:21 +0300

    update js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,32 +1,16 @@
 import React from "react";
-import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Routes, Route } from "react-router-dom";
 
-//Styles
-// import './assets/css/responsive.css';
-// import './assets/css/plugins.css';
 import "./assets/css/font-icons.css";
-// import './assets/css/style.css.map';
-// import './assets/sass/sections/_header.scss';
+
 import "./assets/sass/style.scss";
-// import './assets/sass/sections/_main.scss';
-// import './assets/sass/sections/_footer.scss';
-// import './assets/sass/sections/_breadcrumb.scss';
-// import './assets/sass/sections/_product.scss';
-// import './assets/sass/sections/_widgets.scss';
-// import './assets/sass/global/_global.scss';
-// import "./assets/sass/global/_mixins.scss";
-// import "./assets/sass/global/_variables.scss";
-// import "./assets/sass/global/_gutenberg.scss";
+
 import "./assets/sass/elements/_button.scss";
-// import "./assets/css/elements/_portfoli.css";
-// import "./assets/css/custom.css";
+
 import Login from "./Pages/LoginPage/LoginPage";
 import Home from "./components/Home/Home";
-import Register from "./components/Register/Register";
-import AboutusComp from "./components/AboutUs/AboutusComp";
-import ServiceComp from "./components/Service/ServiceComp";
+
 import ContactUs from "./Pages/contactUsPage/ContactUsPage";
 import RegisterPage from "./Pages/RegisterPage/RegisterPage";
 import ServicePage from "./Pages/ServicePage/ServicePage";
