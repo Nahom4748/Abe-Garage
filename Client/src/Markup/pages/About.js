4677c87889a02ae898de72832165e5fce3701477
Nahom4748 <155730526+Nahom4748@users.noreply.github.com>
2024-09-01 13:06:13 +0300

    NewFrontend

--- Client/src/Markup/pages/About.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AboutUs from "../Components/About_us/AboutUs.jsx";
-
-function About() {
-  return <AboutUs />;
-}
-
-export default About;
--- Client/src/Markup/pages/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import AppointmentForm from "../Components/AppointmentForm/AppointmentForm";
-function Contact() {
-  return <AppointmentForm />;
-}
-
-export default Contact;
